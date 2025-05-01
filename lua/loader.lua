1115f9e91e628bc5498d27ef65e115a76e16e3fa
realalexde <93430311+AleXDE54@users.noreply.github.com>
2025-05-01 21:13:20 +0700

    Create loader.lua

---
+++ lua/loader.lua
@@ -0,0 +1,17 @@
+local HttpService = game:GetService("HttpService")
+
+local url = "https://raw.githubusercontent.com/AleXDE54/real-z-lua/refs/heads/main/lua/realz.lua"
+local success, response = pcall(function()
+    return HttpService:GetAsync(url)
+end)
+
+if success then
+    local func, err = loadstring(response)
+    if func then
+        func()
+    else
+        warn("Ошибка парсинга скрипта: "..err)
+    end
+else
+    warn("Не удалось загрузить скрипт: "..tostring(response))
+end
